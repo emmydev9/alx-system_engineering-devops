4b3868872fd8a1cd7acd4b41f62eb922204e3b7f
emmy <softdevcoder@gmail.com>
2022-11-29 14:16:49 -0500

    Added a functions that the titles of the first 10 hot posts listed for a given subreddit.

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+"""prints the titles of the first 10
+hot posts listed for a given subreddit."""
+import requests
+
+
+def top_ten(subreddit):
+    h = {
+        "User-Agent": "mozilla/5.0"
+    }
+    p = {
+        'limit': 10
+    }
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    response = requests.get(url, headers=h, params=p, allow_redirects=False)
+
+    if response.status_code != 200:
+        print(None)
+        return
+    dict_ = response.json()
+    hot_post = dict_['data']['children']
+    if len(hot_post) == 0:
+        print(None)
+        return
+    else:
+        for post in hot_post:
+            print(post['data']['title'])
